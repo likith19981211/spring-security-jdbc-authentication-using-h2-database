6f852419c0bc5b34727ec26cd99651581f3824d1
likith19981211 <91123593+likith19981211@users.noreply.github.com>
2022-03-03 11:09:06 +0530

    Add files via upload

---
+++ spring-security-jdbc/src/main/java/com/example/springsecurityjdbc/HomeResource.java
@@ -0,0 +1,25 @@
+package com.example.springsecurityjdbc;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home(){
+        return ("<h2>Welcome Guest</h2>");
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return ("<h2>Welcome User</h2>");
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return ("<h2>Welcome Admin</h2>");
+    }
+}
+
